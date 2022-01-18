cc2082b694370c9364fb797a131cefcdb3b0e1c2
Alex Pita <69311722+AlexPizzza@users.noreply.github.com>
2022-01-18 23:26:30 +0200

    first push

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders hello world text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Hello world!/i);
+  expect(headerElement).toBeInTheDocument();
 });
